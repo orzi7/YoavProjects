3fcca88ff9ea7fd8354725cb28dd256d99d20f14
orzi7 <140631583+orzi7@users.noreply.github.com>
2025-12-29 14:57:51 +0200

    Update basic_consistent_speed_servo.ino

--- servo/basic_consistent_speed_servo.ino
+++ servo/basic_consistent_speed_servo.ino
@@ -1,10 +1,43 @@
+#include <Wire.h>
 #include <Servo.h>
+#include <math.h>
+
 Servo myServo;
-int speedValue = 0;
+const int MPU_ADDR = 0x68;
+
+const int MOTOR_PIN = 9;
+
+void motorControl(float KP, int TARGET_ANGLE) {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 6, true);
+
+  if (Wire.available() == 6) {
+    int16_t AcX = Wire.read() << 8 | Wire.read();
+    int16_t AcY = Wire.read() << 8 | Wire.read();
+    int16_t AcZ = Wire.read() << 8 | Wire.read();
+
+    float angle = atan2(AcY, AcZ) * 180 / PI;
+
+    float error = TARGET_ANGLE - angle;
+    
+    int pwmOutput = 1500 + (error * KP);
+
+    pwmOutput = constrain(pwmOutput, 1100, 1900);
+
+    myServo.writeMicroseconds(pwmOutput);
+
+    Serial.print("Angle: "); Serial.print(angle);
+    Serial.print(" | Error: "); Serial.print(error);
+    Serial.print(" | Motor PWM: "); Serial.println(pwmOutput);
+}
 
 void setup() {
-  myServo.attach(9);
-  //stopValue ~1500, maxSpeedFwd ~1900, maxSpeedRev ~1100
+  Serial.begin(9600);
+  Wire.begin();
+
+  myServo.attach(MOTOR_PIN);
   myServo.writeMicroseconds(1500);
   delay(100);
   myServo.writeMicroseconds(1900);
@@ -13,13 +46,12 @@ void setup() {
   delay(500);
   myServo.writeMicroseconds(1500);
   delay(500);
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
 }
 
 void loop() {
-  speedValue = 50;
-  int pwmValue = map(speedValue, -100, 100, 1100, 1900);
-  myServo.writeMicroseconds(pwmValue);
-  delay(2000);
-  myServo.writeMicroseconds(1500);
-  delay(2000);
+  motorControl(15, 0);
 }
