fd318491d365ae12d1ba329e7927cd3c05244e7d
orzi7 <140631583+orzi7@users.noreply.github.com>
2026-04-27 14:10:10 +0300

    Update joystickTwoMotor.ino

--- servo/joystickTwoMotor.ino
+++ servo/joystickTwoMotor.ino
@@ -1,42 +1,50 @@
 #include <Servo.h>
 
 Servo rightServo;
-Servo lefttServo;
+Servo leftServo;
 
 const int joystickYPin = A0;
 const int servoRightPin = 9;
 const int servoLeftPin = 10;
 
+int pwmValue;
+
 void setup() {
   Serial.begin(9600);
   rightServo.attach(servoRightPin);
-  lefttServo.attach(servoRightPin);
+  leftServo.attach(servoLeftPin);
   
   // stopValue ~1100, midSpeedFwd ~1500, maxSpeedFwd ~1900
   rightServo.writeMicroseconds(1500); // MIDDLE
-  lefttServo.writeMicroseconds(1500); // MIDDLE
+  leftServo.writeMicroseconds(1500); // MIDDLE
   delay(100);
   rightServo.writeMicroseconds(1900); // Max Forward
-  lefttServo.writeMicroseconds(1900); // Max Forward
+  leftServo.writeMicroseconds(1900); // Max Forward
   delay(500);
   rightServo.writeMicroseconds(1500); // MIDDLE
-  lefttServo.writeMicroseconds(1500); // MIDDLE
+  leftServo.writeMicroseconds(1500); // MIDDLE
   delay(500);
   rightServo.writeMicroseconds(1100); // STOP
-  lefttServo.writeMicroseconds(1100); // STOP
+  leftServo.writeMicroseconds(1100); // STOP
   delay(500);
 }
 
 void loop() {
   int joyValue = analogRead(joystickYPin);
   
-  int pwmValue = map(joyValue, 0, 1023, 1100, 1900);
+  if (joyValue < 500) {
+    pwmValue = map(joyValue, 500, 0, 1100, 1900);
+  } else if (joyValue > 550) {
+    pwmValue = map(joyValue, 550, 1023, 1100, 1900);
+  } else {
+    pwmValue = 1100;
+  }
   
   rightServo.writeMicroseconds(pwmValue);
-  lefttServo.writeMicroseconds(pwmValue);
+  leftServo.writeMicroseconds(pwmValue);
+  
   Serial.print("Joystick Y: ");
   Serial.print(joyValue);
-  Serial.print("  -->  PWM: ");
+  Serial.print(" --> PWM: ");
   Serial.println(pwmValue);
-  
 }
