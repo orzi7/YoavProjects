eb03b66a37823a4a599057757a587250fbe18f05
orzi7 <140631583+orzi7@users.noreply.github.com>
2026-05-20 12:21:42 +0300

    Update reciver.ino

--- wi fi/reciver.ino
+++ wi fi/reciver.ino
@@ -3,17 +3,16 @@
 #include <math.h>
 #include <RH_ASK.h>
 #ifdef RH_HAVE_HARDWARE_SPI
-#include <SPI.h> // Not actually used but needed to compile
+#include <SPI.h>
 #endif
 
-RH_ASK driver(2000, 2, 9, 10);
+RH_ASK driver(2000, 2, 4, 5);
 
 ServoTimer2 rightServo;
 ServoTimer2 leftServo;
 
 const int MPU_ADDR = 0x68;
 
-const int joystickYPin = A0;
 const int servoRightPin = 9;
 const int servoLeftPin = 10;
 
@@ -33,7 +32,7 @@ int motorControl(String motor, float KP, int TARGET_ANGLE, int basicSpeed) {
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_ADDR, 6, true);
 
-  int pwmOutput;
+  int pwmOutput = basicSpeed;
 
   if (Wire.available() == 6) {
     int16_t AcX = Wire.read() << 8 | Wire.read();
@@ -42,13 +41,17 @@ int motorControl(String motor, float KP, int TARGET_ANGLE, int basicSpeed) {
 
     float angle = atan2(AcY, AcZ) * 180 / PI;
 
+    Serial.print("angle: ");
+    Serial.println(angle);
+
     float error = TARGET_ANGLE - angle;
     
     if (motor == "left") {
-      pwmOutput = (multification * basicSpeed) - (error * KP);
+      pwmOutput = minSpeed + ((basicSpeed - minSpeed) * multification) + (error * KP);
     }
+
     else if (motor == "right") {
-      pwmOutput = basicSpeed + (error * KP);
+      pwmOutput = basicSpeed - (error * KP);
     }
 
     if (pwmOutput < minSpeed) {
@@ -61,43 +64,44 @@ int motorControl(String motor, float KP, int TARGET_ANGLE, int basicSpeed) {
 
     return pwmOutput;
   }
+  
+  return pwmOutput; 
 }
 
 void setup() {
   Serial.begin(9600);
   if (!driver.init()) Serial.println("init failed");
+  
+  Wire.begin();
+  
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);    
+  Wire.endTransmission(true);
+
   rightServo.attach(servoRightPin);
   leftServo.attach(servoLeftPin);
   
   // stopValue ~1100, midSpeedFwd ~1500, maxSpeedFwd ~1900
-  rightServo.write(1500); // MIDDLE
-  leftServo.write(1500); // MIDDLE
-  delay(100);
-  rightServo.write(1900); // Max Forward
-  leftServo.write(1900); // Max Forward
-  delay(500);
-  rightServo.write(1500); // MIDDLE
-  leftServo.write(1500); // MIDDLE
-  delay(500);
   rightServo.write(1100); // STOP
   leftServo.write(1100); // STOP
-  delay(500);
+  delay(3000);
 }
 
 void loop() {
   uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
   uint8_t buflen = sizeof(buf);
 
-    if (driver.recv(buf, &buflen)) {
-
-      String str = "";
-
-      for (int i = 0; i < buflen; i++) {
-        str += (char)buf[i];
-      }
-    
-      pwmValue = str.toInt();
+  if (driver.recv(buf, &buflen)) {
+    String str = "";
+    for (int i = 0; i < buflen; i++) {
+      str += (char)buf[i];
     }
+    pwmValue = str.toInt();
+  }
+
+  //Serial.print("basic speed: ");
+  //Serial.println(pwmValue);
 
   rightServo.write(motorControl("right", 5, 0, pwmValue));
   leftServo.write(motorControl("left", 5, 0, pwmValue));
